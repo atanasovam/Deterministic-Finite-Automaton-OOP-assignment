341d9bff55df6e3bb82195ff6ee33dd69e286568
Milena Atanasova <milena.atanasova@tick42.com>
2019-04-25 17:43:42 +0300

    Added build

--- DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/determinate_finite_automaton.h
+++ DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/determinate_finite_automaton.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef _DETERMINATE_FINITE_AUTOMATION_H
+#define _DETERMINATE_FINITE_AUTOMATION_H
+
+#include <iostream>
+
+class DeterminateFiniteAutomaton {
+public:
+	DeterminateFiniteAutomaton();
+	~DeterminateFiniteAutomaton();
+
+	void print();
+private:
+};
+
+#endif
--- DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/determinate_finite_automaton_char.h
+++ DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/determinate_finite_automaton_char.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef _DETERMINATE_FINITE_AUTOMATION_CHAR_H
+#define _DETERMINATE_FINITE_AUTOMATION_CHAR_H
+
+#include <iostream>
+#include "determinate_finite_automaton.h"
+
+class DeterminateFiniteAutomatonChar { //  : public DeterminateFiniteAutomaton
+public:
+	DeterminateFiniteAutomatonChar();
+	DeterminateFiniteAutomatonChar& operator+(DeterminateFiniteAutomatonChar&);
+	DeterminateFiniteAutomatonChar& operator+(int);
+};
+
+#endif
