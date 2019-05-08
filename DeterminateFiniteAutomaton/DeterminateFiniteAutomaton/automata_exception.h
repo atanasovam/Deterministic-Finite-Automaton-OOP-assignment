407195b7fadae0ee3b09703492331bb7522363a1
Milena Atanasova <milena.atanasova@tick42.com>
2019-05-08 17:20:22 +0300

    Modified the project structure

--- DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/automata_exception.h
+++ DeterminateFiniteAutomaton/DeterminateFiniteAutomaton/automata_exception.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef _DFA_EXCEPTION_H
+#define _DFA_EXCEPTION_H
+
+#include <exception>
+
+class Exception : public std::exception {
+public:
+	Exception(char* errorMessage, const char* details);
+	Exception(char* errorMessage, int detailsCount);
+
+	~Exception() override;
+
+	const char* handleException() const noexcept override;
+
+private:
+	char* errorMessage = new char[256];
+	char* details = new char[128];
+	int detailsCount = -1;
+};
+
+#endif
